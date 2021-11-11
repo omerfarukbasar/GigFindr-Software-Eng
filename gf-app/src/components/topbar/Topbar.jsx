8eeb7b7d90facc2c2cba4e731073a76cc2404b4d
Will Torres <52608541+torrwill@users.noreply.github.com>
2021-11-11 16:15:27 -0500

    Create Nav ("Top") Bar

---
+++ gf-app/src/components/topbar/Topbar.jsx
@@ -0,0 +1,45 @@
+import "./topbar.css"
+import { Search, Person, Chat, Notifications } from "@material-ui/icons"
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">GigFindr</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search className="searchIcon"/>
+          <input placeholder="Search..." 
+          className="searchInput" />
+        </div>
+
+      </div>
+
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Home</span>
+          <span className="topbarLink">Musicians</span>
+          <span className="topbarLink">Venues</span>
+          <span className="topbarLink">Music</span>
+          <span className="topbarLink">Gigs</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">3</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/test-musician.png" alt="Man" className="topbarProfile" />
+      </div>
+    </div>
+  );
+}
