65d0811d35edc4b519b0eb6e4fde705f2e30b627
Will Torres <52608541+torrwill@users.noreply.github.com>
2021-11-15 09:27:33 -0500

    Update Post
    Created the center and bottom section ("post", like, comment)

--- gf-app/src/components/post/Post.jsx
+++ gf-app/src/components/post/Post.jsx
@@ -15,8 +15,19 @@ export default function Post() {
             <MoreVert />
           </div>
         </div>
-        <div className="postCenter"></div>
-        <div className="postBottom"></div>
+        <div className="postCenter">
+          <span className="postText">First Post</span>
+          <img className="postImg" src="assets/test-post.png" alt="violin"/>
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="postLikeImg" src="assets/like.png" alt="like icon"/>
+            <span className="postLikeCount">5 people liked this</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">2 comments</span>
+          </div>
+        </div>
       </div>
     </div>
   )
