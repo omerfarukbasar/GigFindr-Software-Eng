bf49d3ac5cf1e1421c854c6b31457cfe25b049f7
Will Torres <52608541+torrwill@users.noreply.github.com>
2021-11-14 20:05:01 -0500

    Create Sidebar

---
+++ gf-app/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,127 @@
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="card">
+          <img src="/assets/test-musician.png" alt="Man" className="card-img"/>
+          <h1>First Last</h1>
+          <p class="title">@userName</p>
+          <p><button>Visit Profile</button></p>
+        </div>
+        <div className="friendsListDiv">
+          <hr className="sidebarHR" />
+          <ul className="friendsList">
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+
+            <li className="Friend">
+            <img src="/assets/john-doe.jpg" alt="friend img" className="friendsImg"/>
+            <span>John Doe</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+/*
+HTML -- friends list
+
+<ul className="friendsList">
+  <li className="Friend">
+    <img src="./assets/john-doe.png" alt="friend img" className="friendsImg"/>
+    <span className="friendName">John Doe</span>
+  </li>
+  <li></li>
+  <li></li>
+
+CSS
+
+
+*/
