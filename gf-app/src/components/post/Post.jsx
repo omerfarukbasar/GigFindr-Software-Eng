b3197aa1189e8f9157855d9fd4683611a97c49ab
Will Torres <52608541+torrwill@users.noreply.github.com>
2021-11-15 09:07:21 -0500

    Update Post
    Developed the top of the post section (profile picture, user name, post date, and post menu button).

--- gf-app/src/components/post/Post.jsx
+++ gf-app/src/components/post/Post.jsx
@@ -1,9 +1,23 @@
+import { MoreVert } from "@material-ui/icons"
 import "./post.css"
 
 export default function Post() {
   return (
-    <div>
-      post
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img src="/assets/test-musician.png" alt="pfp" className="postProfileImg"/>
+            <span className="postUsername">First Last</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter"></div>
+        <div className="postBottom"></div>
+      </div>
     </div>
   )
 }
