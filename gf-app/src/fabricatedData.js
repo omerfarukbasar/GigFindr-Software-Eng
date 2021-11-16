ea631c7e357a517692cf2ee6d767a2f7f9f3728d
Will Torres <52608541+torrwill@users.noreply.github.com>
2021-11-15 22:57:07 -0500

    Create fabricatedData.js
    Fabricated Data to be integrated into the application

---
+++ gf-app/src/fabricatedData.js
@@ -0,0 +1,145 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "assets/1.png",
+    username: "Will Torres", // Frontend Dev
+  },
+  {
+    id:2,
+    profilePicture: "assets/2.png",
+    username: "Katie McDevitt", // Backend Dev 1
+  },
+  {
+    id:3,
+    profilePicture: "assets/3.png",
+    username: "Omer Basar", // Scrum, Tester
+  },
+  {
+    id:4,
+    profilePicture: "assets/4.png",
+    username: "Brenden Monteleone", // Backend Dev 2
+  },
+  {
+    id:5,
+    profilePicture: "assets/5.png",
+    username: "Anthony Green", // Circa Survive, Saosin
+  },
+  {
+    id:6,
+    profilePicture: "assets/6.png",
+    username: "Tatiana Shmayluk", // Jinjer
+  },
+  {
+    id:7,
+    profilePicture: "assets/7.png",
+    username: "Keith Buckley", // Every Time I Die
+  },
+  {
+    id:8,
+    profilePicture: "assets/8.png",
+    username: "Elijah Witt", // Cane Hill
+  },
+  {
+    id:9,
+    profilePicture: "assets/9.png",
+    username: "Courtney LaPlante", // Spiritbox
+  },
+  {
+    id:10,
+    profilePicture: "assets/10.png",
+    username: "Emma Boster", // Dying Wish
+  },
+]
+
+export const Posts = [
+  {
+    id:1,
+    desc: "I don't know how to play this.",
+    photo: "assets/post-1.png",
+    date: "5 mins ago",
+    userID: 1,
+    like: 34,
+    comment: 2,
+  },
+  {
+    id:2,
+    desc: "New EP, 'A Dream About Love', out now!",
+    photo: "assets/post-2.png",
+    date: "15 mins ago",
+    userID: 5,
+    like: 25,
+    comment: 1,
+  },
+  {
+    id:3,
+    desc: "This record came out 5 years ago. Pisces still slaps.",
+    photo: "assets/post-3.png",
+    date: "1 hour ago",
+    userID: 6,
+    like: 3,
+    comment: 4,
+  },
+  {
+    id:4,
+    desc: "Fav song off Radical?",
+    photo: "assets/post-4.png",
+    date: "3 hours ago",
+    userID: 7,
+    like: 334,
+    comment: 88,
+  },
+  {
+    id:5,
+    desc: "B/W",
+    photo: "assets/post-5.png",
+    date: "1 day ago",
+    userID: 10,
+    like: 32,
+    comment: 1,
+  },
+  {
+    id:6,
+    desc: "Tuned in Drop F# ;)",
+    photo: "assets/post-6.png",
+    date: "1 day ago",
+    userID: 9,
+    like: 45,
+    comment: 12,
+  },
+  {
+    id:7,
+    desc: "I can't believe all these hardcore vocalists don't care about vocal care",
+    photo: "assets/post-7.png",
+    date: "1 day ago",
+    userID: 8,
+    like: 45,
+    comment: 305,
+  },
+  {
+    id:8,
+    desc: "You already know what time it is...",
+    photo: "assets/post-8.png",
+    date: "3 days ago",
+    userID: 2,
+    like: 60,
+    comment: 25,
+  },
+  {
+    id:9,
+    desc: "Sick \m/",
+    photo: "assets/post-9.png",
+    date: "3 days ago",
+    userID: 4,
+    like: 4,
+    comment: 10,
+  },
+  {
+    id:10,
+    desc: "Bought my first instrument from Guitar Center!",
+    photo: "assets/post-10.png",
+    date: "4 days ago",
+    userID: 3,
+    like: 3,
+    comment: 20,
+  },
+]
