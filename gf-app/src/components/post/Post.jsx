cae805302debcc5e77909d997f2a1ae719686512
Will Torres <52608541+torrwill@users.noreply.github.com>
2021-11-15 23:35:02 -0500

    Update Post.jsx
    fetch post data

--- gf-app/src/components/post/Post.jsx
+++ gf-app/src/components/post/Post.jsx
@@ -1,31 +1,34 @@
 import { MoreVert } from "@material-ui/icons"
 import "./post.css"
+import { Users } from "../../fabricatedData";
 
-export default function Post() {
+export default function Post({post}) {
+{/*console.log(post)*/}
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src="/assets/test-musician.png" alt="pfp" className="postProfileImg"/>
-            <span className="postUsername">First Last</span>
-            <span className="postDate">5 mins ago</span>
+            <img src={Users.filter(u=> u.id === post.userID)[0].profilePicture} alt="pfp" className="postProfileImg"/>
+            <span className="postUsername">{Users.filter(u=> u.id === post.userID)[0].username}</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">First Post</span>
-          <img className="postImg" src="assets/test-post.png" alt="violin"/>
+          <span className="postText">{post?.desc}</span>
+          {/* change post type using iframe and conditional rendering, stays as img for now*/}
+          <img className="postImg" src={post.photo} alt="violin"/>
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="postLikeImg" src="assets/like.png" alt="like icon"/>
-            <span className="postLikeCount">5 people liked this</span>
+            <span className="postLikeCount">{post?.like}</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">2 comments</span>
+            <span className="postCommentText">{post?.comment} comments</span>
           </div>
         </div>
       </div>
